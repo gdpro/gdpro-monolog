126d2c69dca8d30b7c3dbc06b2e8088360ceb26a
Gary Gitton <contact@gary.pro>
2014-05-03 19:12:56 +0100

    init src folder

---
+++ src/GdproMonolog/MonologAbstractFactory.php
@@ -0,0 +1,13 @@
+<?php
+namespace GdproMonolog;
+
+use Zend\ServiceManager\AbstractFactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class MonologAbstractFactory implements AbstractFactoryInterface
+{
+
+    public function canCreateService(Service $serviceLocator) {
+
+    }
+}
