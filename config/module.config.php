269057f1d661f51faf719b842375880e300257c7
gary <gary.gitton@gmail.com>
2014-10-06 20:47:26 +0200

    reduce thresold time to 400ms

--- config/module.config.php
+++ config/module.config.php
@@ -3,17 +3,21 @@ return [
     'gdpro_monolog' => [
         'listeners' => [
             'check_slow_response_time' => [
+                'enabled' => true,
                 'logger' => 'slow_response_time',
-                // Response time limit in micro seconds
-                'threshold' => '1000'
+                // Response time limit in micro seconds, this time only take in
+                // Account the application time, not the latency of the network.
+                'threshold' => '400'
             ],
 
             'log_dispatch_error' => [
+                'enabled' => true,
                 'logger' => 'dispatch_error'
             ],
 
             'log_render_error' => [
-                'logger' => 'dispatch_error'
+                'enabled' => true,
+                'logger' => 'render_error'
             ]
         ],
 
