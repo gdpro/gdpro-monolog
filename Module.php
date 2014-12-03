3e01aee7abbd6a1651357f3df9917d63a0660c74
gary <gary.gitton@hotmail.fr>
2014-12-03 07:55:21 +0100

    clean module

--- Module.php
+++ Module.php
@@ -3,11 +3,16 @@ namespace GdproMonolog;
 
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
-use GdproMonolog\Listener\OnDispatchErrorListener;
-use GdproMonolog\Listener\OnRenderErrorListener;
 
+/**
+ * Class Module
+ * @package GdproMonolog
+ */
 class Module
 {
+    /**
+     * @param MvcEvent $e
+     */
     public function onBootstrap(MvcEvent $e)
     {
         $eventManager = $e->getApplication()->getEventManager();
@@ -20,19 +25,25 @@ class Module
         $eventManager->attachAggregate($services->get('gdpro_monolog.listener.log_dispatch_error'));
     }
 
+    /**
+     * @return array
+     */
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
     }
 
+    /**
+     * @return array
+     */
     public function getAutoloaderConfig()
     {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src',
-                ),
-            ),
-        );
+        return [
+            'Zend\Loader\StandardAutoloader' => [
+                'namespaces' => [
+                    __NAMESPACE__ => __DIR__ . '/src/',
+                ]
+            ]
+        ];
     }
 }
