68f066bcafde48dd72bce2e40742836503cd8821
Gary Gitton <gary.gitton@gmail.com>
2016-12-27 11:26:42 +0100

    remove default logger

--- src/Module.php
+++ src/Module.php
@@ -1,51 +1,14 @@
 <?php
 namespace GdproMonolog;
 
-use GdproMonolog\Listener\CheckSlowResponseTimeListener;
-use GdproMonolog\Listener\LogDispatchErrorListener;
-use GdproMonolog\Listener\LogMemoryUsageListener;
-use GdproMonolog\Listener\LogRenderErrorListener;
-use Zend\Mvc\ModuleRouteListener;
-use Zend\Mvc\MvcEvent;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
 
 /**
  * Class Module
- *
  * @package GdproMonolog
  */
-class Module
+class Module implements ConfigProviderInterface
 {
-    /**
-     * @param MvcEvent $e
-     */
-    public function onBootstrap(MvcEvent $e)
-    {
-        $eventManager = $e->getApplication()->getEventManager();
-        $services = $e->getApplication()->getServiceManager();
-        $moduleRouteListener = new ModuleRouteListener();
-        $moduleRouteListener->attach($eventManager);
-
-        $eventManager->attach(MvcEvent::EVENT_FINISH, [
-            $services->get(CheckSlowResponseTimeListener::class),
-            'onFinish'
-        ]);
-
-        $eventManager->attach(MvcEvent::EVENT_RENDER_ERROR, [
-            $services->get(LogRenderErrorListener::class),
-            'onRenderError'
-        ]);
-
-        $eventManager->attach(MvcEvent::EVENT_DISPATCH_ERROR, [
-            $services->get(LogDispatchErrorListener::class),
-            'onDispatchError'
-        ]);
-
-        $eventManager->attach(MvcEvent::EVENT_FINISH, [
-            $services->get(LogMemoryUsageListener::class),
-            'onFinish'
-        ]);
-    }
-
     /**
      * @return array
      */
